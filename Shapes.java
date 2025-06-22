3edd641c1ab132ceac20d42305fe8855802c9231
GreatestDane10 <emailgreatdane@gmail.com>
2025-06-22 20:12:44 +0000

    made shapetrinagle creator

--- Shapes.java
+++ Shapes.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
 public class Shapes{
-  public static void Main(String[] args){
-
+  public static void main(String[] args){
+Scanner scan = new Scanner(System.in);
+System.out.println("enter right triangle hight");
+int height = scan.nextInt();
+for(int i = 0; i<height; i++){
+  int lc = 0;
+ while(lc <i+1){
+  System.out.print("*");
+  lc++;
+ }
+  System.out.println();
+}
   }
 }
