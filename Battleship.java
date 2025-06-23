2ba8d9988eaa9244fd52dc79ee9fec7d277bd978
GreatestDane10 <emailgreatdane@gmail.com>
2025-06-23 15:15:29 +0000

    Setting up board

---
+++ Battleship.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Battleship{
+Scanner scan = new Scanner(System.in);
+    public static void main(String[] args){
+        char[][] board = new char[10][10];//make board
+
+        BoardPrint(board);
+
+    }
+
+    public static void BoardPrint(char[][] board){
+for(int a=0;a<10;a++){
+            for(int b=0;b<10;b++){
+                System.out.print(board[a][b]+" ");
+            }  
+            System.out.println();
+        }
+    }
+
+    public static char[][] SetBoard(){
+        char[][] board=[10][10]
+        for(int a=0;a<10;a++){
+            for(int b =0;b<10;b++){
+                char[]
+            }
+        }
+    }
+}
+//■
+//□
