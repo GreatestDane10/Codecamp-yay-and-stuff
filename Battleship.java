669f1eb1ef6b4d6e4af46c3adcc6c71340920389
GreatestDane10 <emailgreatdane@gmail.com>
2025-06-24 14:02:02 +0000

    Making ship placment checking if in bounds

--- Battleship.java
+++ Battleship.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Battleship {
-    Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
         char[][] P1ShipBoard = new char[10][10];// make board
@@ -15,9 +15,34 @@ public class Battleship {
         P1BoardPrint(P1ViewBoard, P1ShipBoard);
 
     }
-    public static char[][] SetShips(char[][] ShipBoard){
-        System.out.println("which courdenent would you like to start your largest ship of 5 at")
+    public static void SetShipsStats(char[][] ShipBoard){
+    Scanner scan = new Scanner(System.in);
+    ArrayList<Integer> ShipsLeft=new ArrayList<Integer>();
+    ShipsLeft.add(2);
+    ShipsLeft.add(3);
+    ShipsLeft.add(3);
+    ShipsLeft.add(4);
+    ShipsLeft.add(5);
+    While(ShipsLeft.size()>0)
+        System.out.println("You are placing ship")
+        System.out.println("which y coordinate would you like to start your ship of" + ShipSize + "at")
+        int YCoordinate = scan.nextInt();
+        System.out.println("which x coordinate would you like to start your ship of" + ShipSize + "at")
+        int XCoordinate = scan.nextInt();
+        System.out.println("Which derection would you like it going(Up, Down, Left, or Right)");
+        String Direction = scan.nextLine().toLowerCase();
+        InBounds(XCoordinate, YCoordinate, ShipSize, Direction);
         
+    }
+    public static boolean InBounds (int x,int y,int ShipSize,String Direction){
+       for(int d = 0;d<ShipSize;d++){
+            if(Direction==up){
+                if(XCoordinate+(ShipSize-1)<=10){
+                    System.out.println("That is out of bounds, please place again");
+                    
+                }
+            }
+        }
     }
 
     public static void P1BoardPrint(char[][] ViewBoard, char[][] ShipBoard) {
